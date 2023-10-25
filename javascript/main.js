985381b8b5dae86ff507dbc111b548e245f72b81
Aniket Kumar <aa6443@srmist.edu.in>
2023-10-25 21:56:38 +0530

    math methods in Javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,19 @@
-//Math Methods in Javascript 
+//Math Methods in Javascript  and their properties
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI)); //remove all the decimal from PI
+console.log(Math.round(Math.PI)); //return the nearest number from PI
+console.log(Math.ceil(Math.PI));//upper value of the PI
+console.log(Math.floor(Math.PI));//lower value of the PI
+
+console.log(Math.pow(2,10));
+console.log(Math.min(2,5,9));
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+//from 1 to 10 random number generation 
+console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
