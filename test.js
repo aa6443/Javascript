34986d37be5710ccf724c9a57ac5d17f8500b217
Aniket Kumar <aa6443@srmist.edu.in>
2023-10-26 21:03:57 +0530

    data types in js

--- test.js
+++ test.js
@@ -2,4 +2,18 @@
 
 //alert("hello"); //we are using node js not browser 
 
-console.log(3+3)
+//console.log(3+3)   =>//code readability should be maintained 
+
+//number => 2 to power 53
+//big int
+//striing => " "
+//boolean => true or false 
+//null => standalone value 
+//undefined =>    eg- let state; //it is undefined 
+//symbol => unique 
+
+
+//object
+
+console.log(typeof(null));  //null is an object type
+console.log(typeof undefined); //this is also a way to write typeof
