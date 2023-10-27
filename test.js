6e3baa9e1c58c3c70997ed82350c919873a1e5b2
Aniket Kumar <aa6443@srmist.edu.in>
2023-10-27 20:43:02 +0530

    conversions and operations

--- test.js
+++ test.js
@@ -23,4 +23,42 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+// ******************** Operations *******************************************
+
+let value =  3
+let negValue = - value 
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  //power 
+// console.log(2/3);  //division 
+// console.log(2%3);  //remainder   //mostly used in cryptographies 
+
+let str1 = "hello"
+let str2 = " Aniket"
+let str3= str1+str2
+// // console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");  // 1+2 = 3  then 3+"2"=32
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
