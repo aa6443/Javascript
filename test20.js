974be4711c5cb21feeae60174e00bb00f57c1960
Aniket Kumar <aa6443@srmist.edu.in>
2023-12-06 02:24:14 +0530

    truthy ,falsy value ,nullish coalscing operator

--- test20.js
+++ test20.js
@@ -1,14 +1,14 @@
 //truthy and falsy value 
 // if there is string-> assumed to be true value 
 //if empty string -> assumed to be false value 
-const userEmail = "aniket.ai"
+// const userEmail = "aniket.ai"
 
-if(userEmail){
-    console.log("Got User Email");
-}
-else{
-    console.log("Don't have user email");
-}
+// if(userEmail){
+//     console.log("Got User Email");
+// }
+// else{
+//     console.log("Don't have user email");
+// }
 
 //falsy values
 /*  
@@ -25,8 +25,33 @@ false, 0 , -0 ,BigInt , 0n ,"",null , undefined,NaN
 //     console.log("Array is empty ");
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0 ){
-    console.log("Object is empty ");
-}
+// if (Object.keys(emptyObj).length === 0 ){
+//     console.log("Object is empty ");
+// }
+
+//Note
+/*
+false == 0 //true 
+false == "" // true 
+0 == "" //true 
+*/
+
+
+//NULLISH COALESCING OPERATOR (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10   // 10
+// val1 = undefined ?? 15  //15 assigned
+val1 = null ?? 10 ??15 //first value it get is assigned
+
+
+console.log(val1);
+
+//TERNARY OPERATOR 
+// condition ? true : false
+
+const iceTeaPrice =  100 
+iceTeaPrice <= 80 ? console.log("less than 80"):console.log("more than 80");   
