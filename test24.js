bd267065f229b03daa4ec30792707cd87bd9b908
Aniket Kumar <aa6443@srmist.edu.in>
2023-12-06 21:38:36 +0530

    for in loop in js

--- test24.js
+++ test24.js
@@ -1,1 +1,12 @@
 // foreach and for in
+const myObject = {
+    js : 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+} 
+
+//for in can be used for objects
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
